f9a75323e5654e7b36cb113924ce8cd0d1d896b3
2222nit <nitishkamboj8070@gmail.com>
2026-03-04 08:29:29 +0530

    day1

---
+++ Gym/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='bg-zinc-900/70 text-amber-500 h-20 w-full'>
+        <a href="#">Logo</a>
+        <ul>
+          <li><a href=""></a>Gym benefits</li>
+          <li><a href=""></a>Gallery</li>
+          <li><a href=""></a>About</li>
+          <li><a href=""></a>Contact</li>
+        </ul>
+    </div>
+  )
+}
